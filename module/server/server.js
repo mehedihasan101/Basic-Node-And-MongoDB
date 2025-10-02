dafa04010602a12caa48d02d35e143c613f2b8ae
Mehedi Hasan <111342979+mehedihasan101@users.noreply.github.com>
2025-10-02 15:30:27 +0600

    remove .env file

--- module/server/server.js
+++ module/server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config(); 
+//require('dotenv').config(); 
 // Connect to MongoDB
 const db = require("./db.js");
 
@@ -25,7 +25,7 @@ const personRoutes=require('./personRoutes.js');
 app.use('/person',personRoutes); 
 
 //start server prot come from env file otherwise use default 3000
-const PORT=process.env.PORT || 3000 ;
+const PORT= 3000 ;
 app.listen(PORT, () => {
   console.log(`✅ Server is listening on port ${PORT}`);
 });
